a00e1dec69620af92737a2f4116b751727f74c67
zhangyh <1156566771@qq.com>
2019-12-02 21:37:50 +0800

    懒加载及尺寸修改

--- src/core/router.js
+++ src/core/router.js
@@ -4,16 +4,24 @@ import HelloWorld from "../components/HelloWorld"
 
 Vue.use(VueRouter)
 
+function reRouter(to){
+  return () => import("@/components/" + to + ".vue")
+}
+
 const router = new VueRouter({
   routes: [{
       path: '/',
-      component: HelloWorld
+      component: HelloWorld,
+      meta: {
+        title: "张永铧的个人主页"
+      }
     }
   ]
 })
 
 router.beforeEach((to, from, next) => {
-  document.title ="张永铧 &raquo; 为什么坚持，想一想当初";
+  let title = to.meta.title ? to.meta.title : "张永铧的个人主页";
+  document.title = title + " &raquo; 为什么坚持，想一想当初";
   next();
 })
 export default router
